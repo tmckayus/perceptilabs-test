72d98f249d96ccf094c5bc2fa428dd8032b045ab
Trevor McKay <tmckay@redhat.com>
2020-07-09 16:47:19 -0400

    Update tests for robustness and include volume check

--- test.sh
+++ test.sh
@@ -7,19 +7,37 @@ MY_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 os::test::junit::declare_suite_start "$MY_SCRIPT"
 
 checkCreate() {
-  os::cmd::try_until_text "oc get pod -l app.kubernetes.io/name=perceptilabs-frontend -o yaml" 'ready: true'
-  os::cmd::try_until_text "oc get pod -l app.kubernetes.io/name=perceptilabs-core -o yaml" 'ready: true'
+  local corename
+  local frontname
+
+  # Make sure we've only got 1 (in case we ran in a dirty namespace)
+  os::cmd::try_until_text "oc get pods --no-headers -l app.kubernetes.io/name=perceptilabs-core 2> /dev/null | wc -l" '1'
+  os::cmd::try_until_text "oc get pods --no-headers -l app.kubernetes.io/name=perceptilabs-frontend 2> /dev/null | wc -l" '1'
+
+  # Make sure the lists are fully populaed, sometimes "items" is not immediately populated
+  os::cmd::try_until_success "oc get pod -l app.kubernetes.io/name=perceptilabs-core -o jsonpath='{.items[0].metadata.name}'"
+  corename=$(oc get pod -l app.kubernetes.io/name=perceptilabs-core -o jsonpath="{.items[0].metadata.name}")
+  os::cmd::try_until_success "oc get pod -l app.kubernetes.io/name=perceptilabs-frontend -o jsonpath='{.items[0].metadata.name}'"
+  frontname=$(oc get pod -l app.kubernetes.io/name=perceptilabs-frontend -o jsonpath="{.items[0].metadata.name}")
+
+  os::cmd::try_until_text "oc get pod $corename -o jsonpath='{.status.containerStatuses[?(@.name==\"core\")].ready}'" "true"
+  os::cmd::try_until_text "oc get pod $frontname -o jsonpath='{.status.containerStatuses[?(@.name==\"frontend\")].ready}'" "true"
   os::cmd::try_until_success "oc get configmap perceptilabs-license"
   os::cmd::try_until_success "oc get service perceptilabs-core"
   os::cmd::try_until_success "oc get service perceptilabs-frontend"
   os::cmd::try_until_success "oc get route perceptilabs-core"
-  os::cmd::try_until_success "oc get route perceptilabs-frontend"  
-  # test that the selector for the service matches the pod? or test for the creation of an endpoint?
-  # test that the route matches the servicename
+  os::cmd::try_until_success "oc get route perceptilabs-frontend"
+
+  # If the endpoint list is non-null then services successfully mapped to pods
+  os::cmd::try_until_not_text "oc get endpoints perceptilabs-core -o=jsonpath='{.subsets}' | wc -c" '0'
+  os::cmd::try_until_not_text "oc get endpoints perceptilabs-frontend -o=jsonpath='{.subsets}' | wc -c" '0'
 }
 
 checkVolume() {
-    echo check volume not implemented yet
+  local name
+  name=$(oc get pod  -l app.kubernetes.io/name=perceptilabs-core -o jsonpath="{.items[0].metadata.name}")
+  os::cmd::expect_success_and_text "oc get pod $name -o jsonpath='{.spec.volumes[?(@.name==\"core-data-volume\")].persistentVolumeClaim.claimName}'" "perceptilabs-pvc"
+  os::cmd::expect_success_and_text "oc get pod $name -o jsonpath='{.spec.containers[?(@.name==\"core\")].volumeMounts[?(@.name==\"core-data-volume\")]}'" "core-data-volume"
 }
 
 testCreateWithPVC() {
